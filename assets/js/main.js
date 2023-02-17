0111e774f37eb48dba7f6478ca3f5e741987edfe
Gonzalo Fernandois <gfernandois89@gmail.com>
2023-02-17 12:06:29 -0300

    text updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,7 @@ if (checker) {
 if (homeCheck) {
   homeTopCheck.observe(homeCheck);
 }
+window.location.replace("#");
+if (typeof window.history.replaceState === "function") {
+  history.replaceState({}, "", window.location.href.slice(0, -1));
+}
